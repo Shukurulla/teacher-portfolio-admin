5134ea757cf4f55b5ed083b51d57001aa5b249de
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-04-05 15:46:37 +0500

    Start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-export { default as TeacherList } from "./teacherList";
-export { default as TeacterDetail } from "./teacherDetail";
-export { default as JobDetail } from "./jobDetail";
-export { default as FileDetail } from "./fileDetail";
-export { default as NewAchievementsPage } from "./newAchievmentPage";
-export { default as ApprovedFilesPage } from "./approvedFiles.page";
-export { default as NotFound } from "./notFound";
+export { default as TeacherList } from "./TeacherList";
+export { default as TeacterDetail } from "./TeacherDetail";
+export { default as JobDetail } from "./JobDetail";
+export { default as FileDetail } from "./FileDetail";
+export { default as NewAchievementsPage } from "./NewAchievmentPage";
+export { default as ApprovedFilesPage } from "./ApprovedFiles.page";
+export { default as NotFound } from "./NotFound";
 export { default as Dashboard } from "./Dashboard";
